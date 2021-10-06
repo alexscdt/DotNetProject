8bf82d0c97790e2d4eff85567f2231971e591dd6
AlexandreMenu <49943511+AlexandreMenu@users.noreply.github.com>
2021-10-06 11:48:28 +0200

    init

--- obj/Debug/net5.0/RazorDeclaration/_Imports.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/_Imports.razor.g.cs
@@ -13,70 +13,70 @@ namespace BlazorApp
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\projetdotnet\BlazorApp\_Imports.razor"
+#line 1 "D:\projetdotnet\DotNetProject\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\projetdotnet\BlazorApp\_Imports.razor"
+#line 2 "D:\projetdotnet\DotNetProject\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\projetdotnet\BlazorApp\_Imports.razor"
+#line 3 "D:\projetdotnet\DotNetProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\projetdotnet\BlazorApp\_Imports.razor"
+#line 4 "D:\projetdotnet\DotNetProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\projetdotnet\BlazorApp\_Imports.razor"
+#line 5 "D:\projetdotnet\DotNetProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\projetdotnet\BlazorApp\_Imports.razor"
+#line 6 "D:\projetdotnet\DotNetProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\projetdotnet\BlazorApp\_Imports.razor"
+#line 7 "D:\projetdotnet\DotNetProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\projetdotnet\BlazorApp\_Imports.razor"
+#line 8 "D:\projetdotnet\DotNetProject\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\projetdotnet\BlazorApp\_Imports.razor"
+#line 9 "D:\projetdotnet\DotNetProject\_Imports.razor"
 using BlazorApp;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "D:\projetdotnet\BlazorApp\_Imports.razor"
+#line 10 "D:\projetdotnet\DotNetProject\_Imports.razor"
 using BlazorApp.Shared;
 
 #line default
