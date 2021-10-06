e8a5e460bc96aa8feda3f72443c40fa6739a33c9
Alexscdt <49942651+alexscdt@users.noreply.github.com>
2021-10-06 12:10:48 +0200

    init back

---
+++ Api/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjectDotNet.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        
+    }
+}
---
+++ Api/Models/UserContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjectDotNet.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> TodoItems { get; set; }
+    }
+}
