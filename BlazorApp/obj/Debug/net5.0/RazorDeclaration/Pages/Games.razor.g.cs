603a2ade2281d58ac8471113ab66a8510d2315a5
Alexscdt <49942651+alexscdt@users.noreply.github.com>
2021-10-07 22:59:59 +0200

    Connection Front To back

--- BlazorApp/obj/Debug/net5.0/RazorDeclaration/Pages/Games.razor.g.cs
+++ BlazorApp/obj/Debug/net5.0/RazorDeclaration/Pages/Games.razor.g.cs
@@ -91,20 +91,41 @@ using System.Diagnostics;
 #nullable disable
 #nullable restore
 #line 3 "C:\Users\alexs\Documents\Efficom\dotnet\DotNetProject\BlazorApp\Pages\Games.razor"
-using System.Reflection.Metadata;
+using System.Net;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
 #line 4 "C:\Users\alexs\Documents\Efficom\dotnet\DotNetProject\BlazorApp\Pages\Games.razor"
-using System.Threading;
+using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
 #line 5 "C:\Users\alexs\Documents\Efficom\dotnet\DotNetProject\BlazorApp\Pages\Games.razor"
+using System.Reflection.Metadata;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 6 "C:\Users\alexs\Documents\Efficom\dotnet\DotNetProject\BlazorApp\Pages\Games.razor"
+using System.Runtime.CompilerServices;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 7 "C:\Users\alexs\Documents\Efficom\dotnet\DotNetProject\BlazorApp\Pages\Games.razor"
+using System.Threading;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 8 "C:\Users\alexs\Documents\Efficom\dotnet\DotNetProject\BlazorApp\Pages\Games.razor"
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
 #line default
@@ -119,12 +140,12 @@ using Microsoft.AspNetCore.Razor.TagHelpers;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 21 "C:\Users\alexs\Documents\Efficom\dotnet\DotNetProject\BlazorApp\Pages\Games.razor"
+#line 26 "C:\Users\alexs\Documents\Efficom\dotnet\DotNetProject\BlazorApp\Pages\Games.razor"
        
 
     static int Image = 0;
     static int Historique = 0;
-    
+
     private void Start()
     {
         JS.InvokeAsync<object>("start");
@@ -138,12 +159,33 @@ using Microsoft.AspNetCore.Razor.TagHelpers;
     }
     
     [JSInvokable]
-    public static async Task Finish()
+    public static void Finish()
     {
         Console.WriteLine("finis");
         Historique = Image;
         Image = 0;
-        Console.WriteLine("test mec");    
+        Console.WriteLine("test mec");
+    }
+    
+    protected async Task PostScore()
+    {
+        if (Historique != 0)
+        {
+            var postBody = new {    
+                IdUser = "1",
+                Score = Historique,
+                Date =  DateTime.Now
+            };
+            using var response = await HttpClient.PostAsJsonAsync("https://localhost:5001/api/history", postBody);
+        }
+        else
+        {
+            Console.WriteLine("pas mtn");
+        }
+
+
+        // convert response data to Article object
+        // article = await response.Content.ReadFromJsonAsync<Article>();
     }
 
 
@@ -151,6 +193,7 @@ using Microsoft.AspNetCore.Razor.TagHelpers;
 #line hidden
 #nullable disable
         [global::Microsoft.AspNetCore.Components.InjectAttribute] private IJSRuntime JS { get; set; }
+        [global::Microsoft.AspNetCore.Components.InjectAttribute] private HttpClient HttpClient { get; set; }
     }
 }
 #pragma warning restore 1591
